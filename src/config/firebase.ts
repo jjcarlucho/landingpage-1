e50204080633574e861f75f130939cdefb5729d1
jjcarlucho <carluchojonathan2@gmail.com>
2025-05-06 20:50:59 -0400

    fix: actualiza la configuración de Firebase con los valores correctos y agrega analytics

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -2,15 +2,16 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
+import { getAnalytics } from 'firebase/analytics';
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
+  apiKey: "AIzaSyC3yV5JBivrdGP1CeYsfkmd_QKbS0Rtk4Q",
+  authDomain: "myproject-5b844.firebaseapp.com",
+  projectId: "myproject-5b844",
+  storageBucket: "myproject-5b844.firebasestorage.app",
+  messagingSenderId: "591697293552",
+  appId: "1:591697293552:web:12070ac72bd04afccd1235",
+  measurementId: "G-BCN8EPL57B"
 };
 
 // Initialize Firebase
@@ -20,5 +21,6 @@ const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
+export const analytics = getAnalytics(app);
 
 export default app; 
